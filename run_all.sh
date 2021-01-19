e6d49de1fc63cc94cb0d2e7b66bdd5f087c50fa5
Stanislav <stanislavb@saferide.io>
2021-01-19 19:31:24 +0200

    work in progress

--- run_all.sh
+++ run_all.sh
@@ -5,14 +5,14 @@ start=`date +%s`
 ## Scrape search trends
 python social_trends/social_trends.py
 
-### Scrape etoro
-#cd scrapy_projects/etoro/etoro
-#scrapy crawl etoro_dashboard
-#scrapy crawl etoro_investor
-##
-### Scrape marketbeat
-#cd ../../marketbeat/marketbeat
-#scrapy crawl marketbeat
+## Scrape etoro
+cd scrapy_projects/etoro/etoro
+scrapy crawl etoro_dashboard
+scrapy crawl etoro_investor
+#
+## Scrape marketbeat
+cd ../../marketbeat/marketbeat
+scrapy crawl marketbeat
 
 end=`date +%s`
 runtime=$((end-start))
