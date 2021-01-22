b23c0ef5ab5b679f12ced6a8bbc0a8e7293cd12c
Stanislav <stanislavb@saferide.io>
2021-01-22 18:57:09 +0200

    work in progress

--- global_settings.py
+++ global_settings.py
@@ -3,7 +3,7 @@ import os
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-TOP_N_STOCKS = 50  # for marketbeat and google trends
+TOP_N_STOCKS = 1000  # for marketbeat and google trends
 SAVE_EACH_N_ITEMS = 25  # for marketbeat and etoro investor scraper
-ETORO_TOP_N_INVESTORS = 50  # for etoro investor
+ETORO_TOP_N_INVESTORS = 2000  # for etoro investor
 GOOGLE_TRENDS_CHUNK_SIZE = 1  # for google trends
