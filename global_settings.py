8905ca9ce65d64271b2c3ac7e9eae2ed71b4f5d2
Stanislav <stanislavb@saferide.io>
2021-01-24 16:57:56 +0200

    work in progress

--- global_settings.py
+++ global_settings.py
@@ -7,7 +7,7 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 TOP_N_STOCKS = 500  # for marketbeat and google trends
 SAVE_EACH_N_ITEMS = 25  # for marketbeat, etoro and google trends
 ETORO_TOP_N_INVESTORS = 500  # for etoro investor
-GOOGLE_TRENDS_CHUNK_SIZE = 1  # for google trends
+GOOGLE_TRENDS_CHUNK_SIZE = 2  # for google trends
 ALPHAVANTAGE_API_KEY = 'F2050WBAIPD4FC1U'
 FINNHUB_API_KEY = 'c05vlgn48v6v0bd91prg'
 
